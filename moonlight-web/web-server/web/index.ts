42e6adc03e3b3657f0cf3bca4622ee11a799d765
Argon2000 <peas@addovation.com>
2026-06-05 10:05:22 +0200

    Open in fullscreen (YouTube redirect)

--- moonlight-web/web-server/web/index.ts
+++ moonlight-web/web-server/web/index.ts
@@ -104,6 +104,19 @@ class MainApp implements Component {
         this.settingsButton.addEventListener("click", () => this.setCurrentDisplay("settings"))
         this.headerRight.appendChild(this.settingsButton)
 
+        // Fullscreen (Tesla) button — uses YouTube redirect trick to open in fullscreen mode
+        const fullscreenBtn = document.createElement("button")
+        fullscreenBtn.classList.add("fullscreen-launch")
+        fullscreenBtn.title = "Launch in Fullscreen (Tesla)"
+        fullscreenBtn.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="white" width="36" height="36">
+            <path d="M7 14H5v5h5v-2H7v-3zm-2-4h2V7h3V5H5v5zm12 7h-3v2h5v-5h-2v3zM14 5v2h3v3h2V5h-5z"/>
+        </svg>`
+        fullscreenBtn.addEventListener("click", () => {
+            const targetUrl = window.location.origin + (window.location.pathname || "/")
+            window.location.href = "https://www.youtube.com/redirect?q=" + encodeURIComponent(targetUrl)
+        })
+        this.headerRight.appendChild(fullscreenBtn)
+
         // Initialize core components, they will be mounted to appContent
         this.hostList = new HostList(api)
         this.hostList.addHostOpenListener(this.onHostOpen.bind(this))
